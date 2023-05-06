5ee59d9931a20b28efb1e32aa5643090168a6694
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-06 22:57:22 +0600

    use cors to fatch client side

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 
 
-app.get('/' , (res , req)=>{
+const user = [
+    {id: 1 , name: "Buri" , fevurite : "Sabbir"},
+    {id: 2 , name: "Ritu" , fevurite : "Anana"},
+    {id: 3 , name: "Moriom" , fevurite : "Buri"},
+    {id: 4 , name: "Sabbir" , fevurite : "Moriom"}
+   
+]
+
+app.use(cors())
+app.get('/' , (req , res)=>{
     res.send('user management servser is running')
 })
 
+app.get('/user',(req , res)=>{
+    res.send(user)
+})
+
+app.listen(port , ()=>{
+    console.log(`server is running port ${port}`)
+})
