fef02b000b7c71234aa49459b16bd4f8ad84acf7
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-07 11:46:46 +0600

    get user clint side

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const cors = require('cors')
 
 
 const user = [
-    {id: 1 , name: "Buri" , fevurite : "Sabbir"},
-    {id: 2 , name: "Ritu" , fevurite : "Anana"},
-    {id: 3 , name: "Moriom" , fevurite : "Buri"},
-    {id: 4 , name: "Sabbir" , fevurite : "Moriom"}
+    {id: 1 , name: "Buri" , email : "abbir@gmail.com"},
+    {id: 2 , name: "Ritu" , email : "nana@gmail.com"},
+    {id: 3 , name: "Moriom" , email : "uri@gmail.com"},
+    {id: 4 , name: "Sabbir" , email : "oriom@gmail.com"}
    
 ]
 
 app.use(cors())
+app.use(express.json())
+
+
 app.get('/' , (req , res)=>{
     res.send('user management servser is running')
 })
@@ -21,9 +24,13 @@ app.get('/user',(req , res)=>{
     res.send(user)
 })
 
-app.post('/user', (req , res)=>{
-    console.log('server is hitting')
-    console.log(req.body)
+app.post('/user' , (req, res)=>{
+    console.log('Server is hitting')
+    const newUser = req.body;
+    newUser.id = user.length + 1;
+    user.push(newUser)
+    res.send(newUser)
+    console.log(newUser)
 })
 
 app.listen(port , ()=>{
